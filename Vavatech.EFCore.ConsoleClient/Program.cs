43d96df570ebf16b3a1add9d1d8f2c74bda96593
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2019-02-20 11:15:16 +0100

    Add project files.

---
+++ Vavatech.EFCore.ConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vavatech.EFCore.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
